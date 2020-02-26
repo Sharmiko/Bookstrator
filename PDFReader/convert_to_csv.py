7857a54bd170b7bbfa340fa6f036c2022fb9b07f
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2020-02-26 22:37:17 +0400

    minor fix

--- PDFReader/convert_to_csv.py
+++ PDFReader/convert_to_csv.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_file", type=str, help="Location of input pdf file")
     parser.add_argument("--output_file", type=str, help="Location of output file to write into")
-    parser.add_argument("--title_size", type=int, default=20, help="Font size of the title on the content page")
-    parser.add_argument("--body_size", type=int, default=10, help="Font size of the body on the content page")
     parser.add_argument("--start_page", type=int, help="starting page of the range")
     parser.add_argument("--end_page", type=int, help="ending page of the range")
 
@@ -16,12 +14,4 @@ if __name__ == "__main__":
 
     pdf_reader = PDFReader(opt.input_file)
 
-    if opt.title_size != 20:
-        pdf_reader.title_size = opt.title_size
-
-    if opt.body_size != 10:
-        pdf_reader.body_size = opt.body_size
-
-    pdf_reader.to_csv(opt.output_file, opt.start_page, opt.end_page)
-    
-
+    pdf_reader.to_csv(opt.output_file)
